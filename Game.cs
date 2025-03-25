170c2dfd4962af1bc6895f98aa0b6b7a6c19dd46
VisheryV <84638638+VisheryV@users.noreply.github.com>
2025-03-25 19:19:19 +0200

    game code

---
+++ Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumberGame
+{
+    public class Game
+    {
+        private readonly int _targetNumber;
+        private int _attempts;
+
+        public Game(int? predefinedNumber = null)
+        {
+            _targetNumber = predefinedNumber ?? new Random().Next(1, 101);
+            _attempts = 0;
+        }
+
+        public string MakeGuess(int guess)
+        {
+            _attempts++;
+            if (guess < _targetNumber) return "Загадане число більше.";
+            if (guess > _targetNumber) return "Загадане число менше.";
+            return $"Вітаємо! Ви вгадали число {_targetNumber} за {_attempts} спроб.";
+        }
+
+        public int GetAttempts() => _attempts;
+        public int GetTargetNumber() => _targetNumber;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GuessTheNumberGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            var game = new Game();
+            Console.WriteLine("Гра 'Вгадай число'! Введіть число від 1 до 100.");
+
+            while (true)
+            {
+                Console.Write("Ваш варіант: ");
+                if (int.TryParse(Console.ReadLine(), out int guess))
+                {
+                    string result = game.MakeGuess(guess);
+                    Console.WriteLine(result);
+                    if (result.StartsWith("Вітаємо!")) break;
+                }
+                else
+                {
+                    Console.WriteLine("Будь ласка, введіть коректне число.");
+                }
+            }
+        }
+    }
+}
